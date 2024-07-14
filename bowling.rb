1c4c6b241db3f8470a8bc8f48e6526281b089ce8
ocebert31 <153503666+ocebert31@users.noreply.github.com>
2024-07-14 18:00:07 +0200

    test et code terminé pour strike et spare

--- bowling.rb
+++ bowling.rb
@@ -17,21 +17,69 @@ end
 
 class Bowling
     def initialize
-        @throws = []
+        @frames = []
     end
 
     def compute_score
         score = 0
-        @throws.each do |pins|
-            score += pins
+        @frames.each.with_index do |frame, index|
+            score += frame.score
+            next_frame = @frames[index + 1]
+            if frame.strike? && next_frame
+                score += next_frame.score
+            elsif frame.spare? && next_frame
+                score += next_frame.first_throw
+            end
         end
         score
     end
 
     def add_throw(pins)
-        @throws.push(pins)
+        last_frame = @frames.last
+        if last_frame && !last_frame.complete?
+            last_frame.add_second_throw(pins)
+        else
+            new_frame = Frame.new(pins)
+            @frames.push(new_frame)
+        end
+    end
+
+    private 
+    
+    def has_index_plus_1?(index)
+        @throws[index + 1] != nil
     end
 end
+
+class Frame
+    attr_reader :first_throw
+
+    def initialize(pins)
+        @first_throw = pins
+        @second_throw = nil
+    end
+
+    def add_second_throw(pins)
+        @second_throw = pins
+    end
+
+    def complete?
+        @second_throw != nil || strike?
+    end
+
+    def spare?
+        score == 10
+    end
+
+    def strike?
+        first_throw == 10
+    end
+
+    def score
+        first_throw + @second_throw.to_i
+    end
+end
+
 
 assert('0 points when the game was just started') do
     bowling = Bowling.new
@@ -42,4 +90,22 @@ assert('3 pins fallen give 3 points') do
     bowling = Bowling.new
     bowling.add_throw(3)
     bowling.compute_score == 3
+end
+
+assert('spare give 14 points') do
+    bowling = Bowling.new
+    bowling.add_throw(3)
+    bowling.add_throw(7)
+    bowling.add_throw(2)
+    puts bowling.compute_score
+    bowling.compute_score == 14
+end
+
+assert('spare give 28 points') do
+    bowling = Bowling.new
+    bowling.add_throw(10)
+    bowling.add_throw(7)
+    bowling.add_throw(2)
+    puts bowling.compute_score
+    bowling.compute_score == 28
 end
